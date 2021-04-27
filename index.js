d3e3dca6e86211826b1b3960f6e1394a73e9f54d
Markiyan Savka <mark.savka@ekreative.com>
2021-04-27 15:07:31 +0300

    add real_json

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = 8000
+const cors = require('cors')
+const path = require('path')
+
+const jsonData = require('./api/router/json.json')
+
+app.use( cors() );
+
+const dir = path.join(__dirname, 'public');
+
+app.use(express.static(dir));
+
+app.get('/', (req, res) => {
+   res.send('Hello World!')
+})
+
+app.get('/item', (req, res) => {
+   res.send(jsonData)
+})
+
+app.listen(port, () => {
+   console.log(`Example app listening at http://localhost:${port}`)
+})
